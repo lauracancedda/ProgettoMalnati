4f5c72be9b12aacff7e2158fbcf915f2a2443742
lucio ciraci <lucio.ciraci94@gmail.com>
2019-03-08 17:11:34 +0100

    FormSharing Push

---
+++ progetto/FormSharing.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Progetto
+{
+    partial class FormSharing
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.checkBox1);
+            this.flowLayoutPanel1.Controls.Add(this.pictureBox1);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(236, 157);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(171, 163);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(3, 3);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Users Online";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(27, 281);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Send File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 311);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // FormSharing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "FormSharing";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.FormSharing_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
---
+++ progetto/FormSharing.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Progetto
+{
+    public partial class FormSharing : Form
+    {
+        private Dictionary<IPAddress, value> OnlineUsers;
+        //private Dictionary<IPAddress, value> SelectedUsers;
+        private List<visualization> userselected;
+        struct visualization
+        {
+            public FlowLayoutPanel layout;
+            public PictureBox picture;
+            public Label label;
+            public CheckBox checkbox;
+            public value val;
+
+        }
+        public FormSharing(Dictionary<IPAddress, value> OnlineUsers1)
+        {
+            InitializeComponent();
+            OnlineUsers = OnlineUsers1;
+            userselected = new List<visualization>();
+            // Lock Map - Read Map - Create form for each element into the map
+            foreach (KeyValuePair<IPAddress, value> entry in OnlineUsers1)
+            {
+                //entry.Value...
+                // visualizzare gli users, quindi crere il form adatto
+                Console.WriteLine(entry.Value.name.ToString());
+                FlowLayoutPanel flp1 = new FlowLayoutPanel();
+                CheckBox cb1 = new CheckBox();
+                cb1.Text = entry.Value.name.ToString();
+                cb1.Enabled = true;
+                cb1.Visible = true;
+
+
+                flp1.Controls.Add(cb1); // checksbox1
+                flp1.TabIndex = 1;
+            }
+        }
+
+        private void FormSharing_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x, y;
+            x = 1; y = 1;//Per poter disporre i vari layout utente
+            foreach (KeyValuePair<IPAddress, value> entry in OnlineUsers)
+            {
+                visualization v1;
+
+                //Nome UTente
+                v1.label = new Label();
+                v1.label.Text = entry.Value.name;
+                v1.label.Name = entry.Value.name;
+
+                //Image Utente
+                v1.picture = new PictureBox();
+                v1.picture.Name = "Im1";
+                v1.picture.Image = entry.Value.photo;
+                v1.picture.SizeMode = PictureBoxSizeMode.StretchImage;
+                v1.picture.Size = new Size(75, 75);
+
+                //Checkbox Utente
+                v1.checkbox = new CheckBox();
+                v1.checkbox.Name = entry.Value.name;
+
+                //Layout
+                v1.layout = new FlowLayoutPanel();
+                v1.layout.Location = new Point(x, y);
+                v1.layout.Size = new Size(100, 150);
+                v1.layout.BorderStyle = BorderStyle.Fixed3D;
+                v1.layout.Controls.Add(v1.label);
+                v1.layout.Controls.Add(v1.picture);
+                v1.layout.Controls.Add(v1.checkbox);
+
+                //value Utente
+                v1.val = entry.Value;
+                userselected.Add(v1);
+                //this.Controls.Add(v1.label);
+                //this.Controls.Add(v1.picture);
+                //this.Controls.Add(v1.checkbox);
+                this.Controls.Add(v1.layout);
+
+                x += 100;
+                if (x >= 1000)
+                {
+                    x = 0;
+                    y += 100;
+                }
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string s = "";
+            foreach (visualization x in userselected)
+            {
+                if (x.checkbox.Checked)
+                {
+                    s += x.val.name + x.val.ip.ToString() + " - ";
+                    int porta = 150;
+                    bool r = true;
+                    //SGANGIARE THREDD PER OGNI INVIO FILE!!!
+                    TCPClass tcp = new TCPClass();
+                    //tcp.Connect(x.val.ip, porta);
+                    //openFileDialog1.ShowDialog();
+                    //var result = dialog.ShowDialog();
+                    //if (result != DialogResult.OK)
+                    //    return;//
+                    //byte[] file = System.IO.File.ReadAllBytes(openFileDialog1.FileName);
+                    //tcp.SendFileBuffered(file,ref r);
+                    //CREARE FORM PER SAPERE STATO CONNESSIONE usando il metodo di TCPCLASS StatusSendFile()
+                }
+            }
+            label1.Text = s;
+            
+        }
+    }
+}
